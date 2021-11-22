f80d316fdddd3a22d07f6fcec9e6486166f7297e
Ana <anaurrig@gmail.com>
2021-11-22 21:02:51 +0100

    feat: add spain json

--- src/components/AirportConnections.jsx
+++ src/components/AirportConnections.jsx
@@ -2,13 +2,6 @@ import { Fragment } from 'react';
 import mapOfUSA from '../vega/airportConnections/mapOfUSA.json';
 import { Vega } from 'react-vega';
 
-
-import airports from '../data/airports.csv'
-import flightsAriport from '../data/flights-airport.csv'
-
-
-
-
 function AirportConnections() {
 
  function handleHover(...args){
